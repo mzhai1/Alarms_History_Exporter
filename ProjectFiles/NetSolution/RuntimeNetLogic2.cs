2ff453bf192a4c044ba90bbe78fee3846779b17f
Melody Chenxi Zhai <mzhai2@ra.rockwell.com>
2023-05-11 10:09:27 +0800

    rn

---
+++ ProjectFiles/NetSolution/RuntimeNetLogic2.cs
@@ -0,0 +1,29 @@
+#region Using directives
+using System;
+using UAManagedCore;
+using OpcUa = UAManagedCore.OpcUa;
+using FTOptix.CoreBase;
+using FTOptix.Store;
+using FTOptix.EventLogger;
+using FTOptix.NativeUI;
+using FTOptix.Alarm;
+using FTOptix.UI;
+using FTOptix.NetLogic;
+using FTOptix.SQLiteStore;
+using FTOptix.HMIProject;
+using FTOptix.Retentivity;
+using FTOptix.Core;
+#endregion
+
+public class RuntimeNetLogic2 : BaseNetLogic
+{
+    public override void Start()
+    {
+        // Insert code to be executed when the user-defined logic is started
+    }
+
+    public override void Stop()
+    {
+        // Insert code to be executed when the user-defined logic is stopped
+    }
+}
