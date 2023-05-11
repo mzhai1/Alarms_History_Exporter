e5cd6a249d1f7d637567b366961609cf36298632
mzhai1 <62233841+mzhai1@users.noreply.github.com>
2023-05-11 10:11:56 +0800

    Update RuntimeNetLogic2.cs

--- ProjectFiles/NetSolution/RuntimeNetLogic2.cs
+++ ProjectFiles/NetSolution/RuntimeNetLogic2.cs
@@ -2,20 +2,20 @@
 using System;
 using UAManagedCore;
 using OpcUa = UAManagedCore.OpcUa;
-using FTOptix.CoreBase;
-using FTOptix.Store;
-using FTOptix.EventLogger;
-using FTOptix.NativeUI;
-using FTOptix.Alarm;
-using FTOptix.UI;
-using FTOptix.NetLogic;
-using FTOptix.SQLiteStore;
 using FTOptix.HMIProject;
+using FTOptix.NetLogic;
+using FTOptix.UI;
+using FTOptix.NativeUI;
+using FTOptix.EventLogger;
+using FTOptix.SQLiteStore;
+using FTOptix.Store;
 using FTOptix.Retentivity;
+using FTOptix.CoreBase;
 using FTOptix.Core;
+using System.IO;
 #endregion
 
-public class RuntimeNetLogic2 : BaseNetLogic
+public class RuntimeNetLogic1 : BaseNetLogic
 {
     public override void Start()
     {
@@ -25,5 +25,28 @@ public class RuntimeNetLogic2 : BaseNetLogic
     public override void Stop()
     {
         // Insert code to be executed when the user-defined logic is stopped
+    }
+
+    [ExportMethod]
+    public void CheckExportedFile()
+    {
+        var csvPathVariable = LogicObject.Owner.GetObject("AlarmsHistoryExporter").GetVariable("CSVPath");
+        var csvPath = new ResourceUri(csvPathVariable.Value).Uri;
+        if (!File.Exists(csvPath))
+        {
+            Log.Error("File " + csvPath + " does not exist");
+            return;
+        }
+
+        Log.Info("Dumping file " + csvPath + " contents");
+        using (var reader = new StreamReader(csvPath))
+        {
+            while (!reader.EndOfStream)
+            {
+                var line = reader.ReadLine();
+                Log.Info("\t" + line);
+            }
+        }
+        Log.Info("End of file dump");
     }
 }
